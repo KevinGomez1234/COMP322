23e86d20b9b1b2e8d34aa02bc9d57253462eae94
Kevin <you@example.com>
2020-03-14 11:45:56 -0700

    added comments and refactored

--- lab2/launch.c
+++ lab2/launch.c
@@ -9,30 +9,38 @@ void logic();
 int main(int argc, char** argv)
 {
 	//to ignore the unused parameter warning.
-	(void)argc;
-	logic(argv);
+	logic(argc, argv);
 	return 0;
 }
 
-
-void logic(char** argv)
+void logic(int argc, char** argv)
 {
 	int status = 0;
 	int returned;
-	pid_t process_1 = fork();
-	
-	// we are the child, execute the argument.
-	if((int)process_1 == 0)
+
+	//improper usage case. need at least 1 arg. 
+	if (argc < 2)
 	{
-		execve(argv[1], argv+1, NULL);
-		exit(0);
+		printf("Improper usage, use like this: ./launch <cmd>\n");
 	}
-	//parent prints pid and return status
-	else if((int)process_1 > 0)
-	{	
-		fprintf(stderr ,"%s: $$ = %d\n", argv[1], process_1);
-		waitpid(process_1, &status, 0);
-		returned = WEXITSTATUS(status);
-		fprintf(stderr, "%s: $? = %d\n", argv[1], returned);
+	//run the program
+	else
+	{
+		pid_t process_1 = fork();
+		// we are the child, execute the argument.
+		if((int)process_1 == 0)
+		{
+			execve(argv[1], argv+1, NULL);
+			exit(0);
+		}
+		//parent prints pid and return status
+		else if((int)process_1 > 0)
+		{	
+			fprintf(stderr ,"%s: $$ = %d\n", argv[1], process_1);
+			waitpid(process_1, &status, 0);
+			returned = WEXITSTATUS(status);
+			fprintf(stderr, "%s: $? = %d\n", argv[1], returned);
+		}
 	}
+
 }
--- lab2/tube.c
+++ lab2/tube.c
@@ -22,14 +22,10 @@ void logic(int argc, char* argv[])
 	int split = indexAtComma(argc, argv);
 	//improper usage case
 	if(split < 0)
-	{
 		printf("Improper Usage, use like this: ./tube <arg1> , <arg2>\n");
-	}
 	// parse and run the args
 	else
-	{
 		runArgs(argc, argv, split);
-	}
 }
 
 //find the index where there is a comma
@@ -37,17 +33,14 @@ int indexAtComma(int argc, char* argv[])
 {	
 	int count = 0;
 	//parse the command line args to find the comma
-	while(count < argc)
-	{
+	while(count < argc) {
 		if(argv[count][0] == ',')
 			break;
 		count++;
 	}	
-	//if there are less than 3 args you're missing a comma or an argument, or if one side of comma has no args you're missing an argument.
+	//if there are less than 3 args you're missing a comma or an argument, or if one side of comma has no args you're missing an argument, or if no comma was found you;re missing an arg.
 	if (argc < 3 || count == argc-1 || count == argc)
-	{
 		count = -1;
-	}
 	return count;
 }
 
@@ -60,14 +53,12 @@ void runArgs(int argc, char* argv[], int count)
 	char* args2[argc-count];
 	int counter_2 = 0;
 	//split args and put them into seperate char arrays, to use on execve().
-	for(int i = 1; i<count; i++)
-	{
+	for(int i = 1; i<count; i++) {
 		args1[i-1] = argv[i];
 	}	
 	//make the last element of this split arg NULL
 	args1[count-1] = NULL;
-	for(int j = count+1; j<argc; j++)
-	{
+	for(int j = count+1; j<argc; j++) {
 		args2[counter_2] = argv[j];
 		counter_2++;
 	}
@@ -77,35 +68,29 @@ void runArgs(int argc, char* argv[], int count)
 	pipe_return_value = pipe(the_pipe);
 	//fork a child
 	childA = fork();
+	
 	if(pipe_return_value == -1)
-	{
 		perror("initializing pipe did not work");
-	}
-	else
-	{
+	else {
 		//child A process outputs to stdout using args1
-		if((int)childA == 0)
-		{
-			//close end of pipe not used, redirect stdin, and run arg.
+		if((int)childA == 0) {
+			//close the end of the pipe not used, redirect pipe to stdout, and run arg.
 			close(the_pipe[0]);
 			dup2(the_pipe[1], STDOUT_FILENO);
 			execve(args1[0], args1, NULL);
 		}
-		else if((int)childA > 0)
-		{			
+		else if((int)childA > 0) {			
 			//fork another child
 			childB = fork();
 			//child B will read from the stdin and use it to run a second command
-			if((int)childB == 0)
-			{
+			if((int)childB == 0) {
 				//close end of pipe not used, redirect stdin, and run arg.
 				close(the_pipe[1]);
 				dup2(the_pipe[0], STDIN_FILENO);
 				execve(args2[0], args2, NULL);
 			}
 			//parent process prints process info, waits for children, and closes end of pipes.
-			else if((int)childB > 0)
-			{
+			else if((int)childB > 0) {
 				fprintf(stderr, "%s $$ =  %d\n", args1[0],(int)childA);
 				fprintf(stderr, "%s $$ = %d\n", args2[0],(int)childB);
 				waitpid((int)childA, &statusA, 0);
