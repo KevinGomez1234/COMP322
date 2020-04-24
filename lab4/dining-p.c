d1f33088971169f7944e5cc1aef0afcbf0553ec4
Kevin <you@example.com>
2020-04-23 17:33:57 -0700

    finished part 1 working on part 2: circular wait

--- lab4/dining-p.c
+++ lab4/dining-p.c
@@ -16,14 +16,14 @@ void thinking();
 void eat_and_think_cycle();
 void initializeSemapores();
 void signalHandler();
+
 int phil;
 int seats;
 int cycles;
-
 //semaphor for each chopstick left and right chopstick
-sem_t* chopsticks[100];
-char buffer1[100];
-char buffer2[100];
+sem_t* chopsticks[2];
+char buffer1[50];
+char buffer2[50];
 
 int main(int argc, char* argv[])
 {
@@ -32,8 +32,18 @@ int main(int argc, char* argv[])
 	seats = atoi(argv[1]);
 //curent philosopher we start at 1
 	phil = atoi(argv[2]) - 1;
-	initializeSemapores();
-	eat_and_think_cycle();
+	//
+	if(phil+1 > seats)
+	{
+		printf("not enough seats\n");
+		exit(1);
+	}
+	else
+	{
+		initializeSemapores();
+		eat_and_think_cycle();
+	}
+
 	return 0;
 }
 
@@ -45,14 +55,18 @@ void initializeSemapores()
 	char phil_chop_index_2[32];
 	sprintf(phil_chop_index_1, "%d", phil);
 	sprintf(phil_chop_index_2, "%d", (phil+1)%seats);
-	sprintf(buffer1, "/c_s%s", phil_chop_index_1);
-	sprintf(buffer2, "/c_s%s", phil_chop_index_2);
-	chopsticks [phil] = sem_open(buffer1, O_CREAT, 0666);
-	chopsticks [(phil+1)%seats] = sem_open(buffer2, O_CREAT, 0666);
-	if(chopsticks[phil] == SEM_FAILED)
-		perror("chop[left]");
-	if(chopsticks[(phil+1)%seats] == SEM_FAILED)
-		perror("chop[right]");
+	//we have a char buffer that maps to the sem name
+	buffer1[0] = '/'; buffer1[1] = 'z'; strcat(buffer1, phil_chop_index_1);
+	buffer2[0] = '/'; buffer2[1] = 'z'; strcat(buffer2, phil_chop_index_2);
+
+	//open the buffers
+	chopsticks[0] = sem_open(buffer1, O_CREAT, 0666, 1);
+	chopsticks[1] = sem_open(buffer2, O_CREAT, 0666, 1);
+	//check chopsticks output
+	if(chopsticks[0]==SEM_FAILED)
+		perror("chop[0] failed");
+	if(chopsticks[1]==SEM_FAILED)
+		perror("chop[1] failed");	
 }
 
 void eat_and_think_cycle()
@@ -62,11 +76,11 @@ void eat_and_think_cycle()
 //open up our semaphores to be used with
 	do
 	{
-		sem_wait(chopsticks[phil]);
-		sem_wait(chopsticks[(phil+1)%seats]);
+		sem_wait(chopsticks[0]);
+		sem_wait(chopsticks[1]);	
 		eat();
-		sem_post(chopsticks[(phil+1)%seats]);
-		sem_post(chopsticks[(phil+1)%seats]);
+		sem_post(chopsticks[0]);
+		sem_post(chopsticks[1]);
 		cycles++;
 		thinking();
 	}while(1);
@@ -77,12 +91,10 @@ void signalHandler(int mysignal)
 	signal(15, signalHandler);
 	if(mysignal == 15)
 	{
-		sem_close(chopsticks[phil]);
-		sem_close(chopsticks[(phil+1)%seats]);
+		sem_close(chopsticks[0]);
+		sem_close(chopsticks[1]);
 		sem_unlink(buffer1);
 		sem_unlink(buffer2);
-		sem_destroy(chopsticks[phil]);
-		sem_destroy(chopsticks[(phil+1)%seats]);
 	}
 	fprintf(stderr, "Philosopher%d completed %d cycles\n", phil+1, cycles);
 	exit(0);
